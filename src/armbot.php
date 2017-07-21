d31ef7c9f793e11d9ae4a830d658128f89dbe27b
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 20:18:58 +0700

    m

--- src/armbot.php
+++ src/armbot.php
@@ -1,10 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder ;
-use \LINE\LINEBot\MessageBuilder\TemplateActionBuilder\MessageTemplateActionBuilder ;
-use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder ;
-use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder ;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder ;
+use LINE\LINEBot\MessageBuilder\TemplateActionBuilder\MessageTemplateActionBuilder ;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder ;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder ;
 
 $token = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwuoEcNqgwiJhZTHen75QixKLah1ttM+Ms6snrxNSPcYV+284HLUEEbflnJuN5xHBCsvsOjaqXyoCW3lHu8uWgMwzL5pgPjAdB04t89/1O/w1cDnyilFU=' ;
 $secret = '255befc1f82d6539c481e5f593e92517' ;
@@ -13,8 +13,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
 
 $body = file_get_contents('php://input');
-$hash = hash_hmac('sha256', $body, $secret, true);
-$signature = base64_encode($hash);
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 $data = $bot->parseEventRequest($body, $signature);
 
@@ -28,7 +27,14 @@ foreach ($data as $event) {
 
 	$reply_token = $event->getReplyToken();
 	//$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello its me');
-	$response = $bot->replyMessage( $reply_token , 'ควยไร');  
+	$response = $bot->replyMessage( $reply_token , 
+                    new TemplateMessageBuilder(
+                        'Confirm alt text',
+                        new ConfirmTemplateBuilder('Do it?', [
+                            new MessageTemplateActionBuilder('Yes', 'Yes!'),
+                            new MessageTemplateActionBuilder('No', 'No!'),
+                        ])
+                    ));  
 }
  
 

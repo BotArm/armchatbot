44b7f79c89f9155e4d6acf0037bcafea5d4da6b8
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 13:55:35 +0700

    hello

--- src/index.php
+++ src/index.php
@@ -1,5 +1,34 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once('./LINEBot.php');
 
-echo "hello";
+$channelAccessToken = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwuoEcNqgwiJhZTHen75QixKLah1ttM+Ms6snrxNSPcYV+284HLUEEbflnJuN5xHBCsvsOjaqXyoCW3lHu8uWgMwzL5pgPjAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '255befc1f82d6539c481e5f593e92517';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};

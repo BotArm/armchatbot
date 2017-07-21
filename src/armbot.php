22977bdf619018813dc59dd83029fa6fd155e91f
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 23:02:54 +0700

    m

--- src/armbot.php
+++ src/armbot.php
@@ -2,6 +2,9 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder ;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder ;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder ;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder ;
 
 $token = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwuoEcNqgwiJhZTHen75QixKLah1ttM+Ms6snrxNSPcYV+284HLUEEbflnJuN5xHBCsvsOjaqXyoCW3lHu8uWgMwzL5pgPjAdB04t89/1O/w1cDnyilFU=' ;
 $secret = '255befc1f82d6539c481e5f593e92517' ;
@@ -13,7 +16,8 @@ $body = file_get_contents('php://input');
 $events = json_decode($body, true);
 
 foreach ($events['events'] as $event) {
-	$MessageBuilder = new TextMessageBuilder(json_encode( (object) $event['message'] ) );
+	$MessageBuilder = new TemplateMessageBuilder('ทดสอบ', new ConfirmTemplateBuilder('ทดสอบควยไร', 
+		[ new MessageTemplateActionBuilder('Yes', 'Yes') , new MessageTemplateActionBuilder('No', 'No') ]) );
 	//$MessageBuilder = json_encode( (object) $event['message'] );
 	$response = $bot->replyMessage( $event['replyToken'] , $MessageBuilder);  
 }

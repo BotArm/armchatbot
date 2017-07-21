9356bf9a981cda03b05e097124712f949b80684f
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 13:59:22 +0700

    fix error

--- src/LINEBot.php
+++ src/LINEBot.php
@@ -65,6 +65,38 @@ class LINEBot
      * @param string $userId The user ID to retrieve profile.
      * @return Response
      */
+
+    public function parseEvents()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            error_log("Method not allowed");
+            exit();
+        }
+
+        $entityBody = file_get_contents('php://input');
+
+        if (strlen($entityBody) === 0) {
+            http_response_code(400);
+            error_log("Missing request body");
+            exit();
+        }
+
+        if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
+            http_response_code(400);
+            error_log("Invalid signature value");
+            exit();
+        }
+
+        $data = json_decode($entityBody, true);
+        if (!isset($data['events'])) {
+            http_response_code(400);
+            error_log("Invalid request body: missing events property");
+            exit();
+        }
+        return $data['events'];
+    }
+    
     public function getProfile($userId)
     {
         return $this->httpClient->get($this->endpointBase . '/v2/bot/profile/' . urlencode($userId));

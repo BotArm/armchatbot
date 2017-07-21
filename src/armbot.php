3460b338b263ee835c80517830a991787cfd1cab
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 20:40:21 +0700

    m

--- src/armbot.php
+++ src/armbot.php
@@ -8,24 +8,18 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);
 
 $body = file_get_contents('php://input');
-if(isset($body)){
-	$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-}
 
-
-$data = $bot->parseEventRequest($body, $signature);
-
-foreach ($data as $event) {
+/*foreach ($data as $event) {
 	//$yes = new \LINE\LINEBot\MessageBuilder\TemplateActionBuilder\MessageTemplateActionBuilder('yes','เคยสมัครแล้ว') ;
 	//$no = new \LINE\LINEBot\MessageBuilder\TemplateActionBuilder\MessageTemplateActionBuilder("no","ยังไม่เคยสมัคร") ;
 
-	/*$buttonTemplate = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('คุณเคยสมัครแล้วหรือยัง', $yes );
-	$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('this is a confirm template', $buttonTemplate ) ;*/
+	$buttonTemplate = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('คุณเคยสมัครแล้วหรือยัง', $yes );
+	$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('this is a confirm template', $buttonTemplate ) ;
 	//$response = $bot->replyMessage($event['replyToken'], $MessageBuilder);  
 
 	$reply_token = $event->getReplyToken();
 	//$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello its me');
 	$response = $bot->replyMessage( $reply_token , 'ควยไร');  
-}
+}*/
  
 

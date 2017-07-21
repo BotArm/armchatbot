fe2dc94bcfa79aeb3d01109714619ed5eb3e11ea
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 14:21:23 +0700

    s

--- src/index.php
+++ src/index.php
@@ -6,29 +6,3 @@ $channelAccessToken = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwu
 $channelSecret = '255befc1f82d6539c481e5f593e92517';
 
 $client = new LINEBot($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
-    }
-};

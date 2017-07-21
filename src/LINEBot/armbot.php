071b97ff4c90e61ca7c49829d14e58e39bce3782
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 14:50:11 +0700

    s

---
+++ src/LINEBot/armbot.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$token = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwuoEcNqgwiJhZTHen75QixKLah1ttM+Ms6snrxNSPcYV+284HLUEEbflnJuN5xHBCsvsOjaqXyoCW3lHu8uWgMwzL5pgPjAdB04t89/1O/w1cDnyilFU=' ;
+$secret = '255befc1f82d6539c481e5f593e92517' ;
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $secret]);

c5ae1ce85d7583ffbe38ad4c89ad95b92c0d1ed9
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-21 14:25:04 +0700

    s

--- src/index.php
+++ src/index.php
@@ -1,7 +1,5 @@
 <?php
 
-require('./LINEBot.php'); ;
-
 $channelAccessToken = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwuoEcNqgwiJhZTHen75QixKLah1ttM+Ms6snrxNSPcYV+284HLUEEbflnJuN5xHBCsvsOjaqXyoCW3lHu8uWgMwzL5pgPjAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '255befc1f82d6539c481e5f593e92517';
 

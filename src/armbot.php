46ffbe8dde4bf0a8f61e906dc3f16c658bf29419
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-23 14:50:49 +0700

    s

--- src/armbot.php
+++ src/armbot.php
@@ -77,7 +77,9 @@ foreach ($events['events'] as $event) {
     	$result = $conn->query($sql) ;
     	if ($result->num_rows > 0) {
     		while($row = $result->fetch_assoc()) {
-    			checkSession($row["log_LastMsg"], $row["log_Session"]) ;
+    			$MessageBuilder = new TextMessageBuilder('LastMsg : '.$row["log_LastMsg"]." Session : ".$row["log_Session"]) ;
+    			$bot->replyMessage( $event['replyToken'] , $MessageBuilder);  
+    			//checkSession($row["log_LastMsg"], $row["log_Session"]) ;
     		}
     	} else {
     		$MessageBuilder = new TextMessageBuilder('อย่าพิมอย่างอื่นไอหน่อม') ;

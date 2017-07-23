e919d514839b12e0e163a79ba8907c2637b1a374
itangx <pornpawit.1@mail.kmutt.ac.th>
2017-07-23 14:35:09 +0700

    s

--- src/armbot.php
+++ src/armbot.php
@@ -18,6 +18,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
+mysqli_set_charset($conn,"utf8");
+
 $token = 'QFW5zx4qTkWfWsQKNlaOf5lDCgFTNt+wKV8rw5P/8UlQxbOqNarlInIwuoEcNqgwiJhZTHen75QixKLah1ttM+Ms6snrxNSPcYV+284HLUEEbflnJuN5xHBCsvsOjaqXyoCW3lHu8uWgMwzL5pgPjAdB04t89/1O/w1cDnyilFU=' ;
 $secret = '255befc1f82d6539c481e5f593e92517' ;
 
